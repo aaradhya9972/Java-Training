b64974950782c93d4090b7066273f2163648b474
Aaradhya <144376341+aaradhya9972@users.noreply.github.com>
2026-06-03 15:43:07 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,33 @@
+public class BinarySearch {
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 45, 67, 91};
+        int target = 23;
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1)
+            System.out.println("Element " + target + " found at index " + result);
+        else
+            System.out.println("Element " + target + " not found in the array");
+    }
+} {
+
+}
